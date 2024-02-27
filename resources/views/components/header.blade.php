d733c40787dece3970118038c774302370daa4c8
in-th3-l00p <indianucode0303@gmail.com>
2024-02-27 21:14:08 +0200

    dashboard when authenticated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,24 +5,23 @@
             <p class=" my-auto ml-2 text-2xl">Infopubele.ro</p>
         </div>
         <div class="">
-                        <x-bladewind.dropmenu>
-                            <x-bladewind.dropmenu-item>
-                                <p class="text-black">Acasa</p>
-                            </x-bladewind.dropmenu-item>
-                            <x-bladewind.dropmenu-item>
-                                <p class="text-black">Despre noi</p>
-                            </x-bladewind.dropmenu-item>
-                            <x-bladewind.dropmenu-item>
-                                <p class="text-black">Contact</p>
-                            </x-bladewind.dropmenu-item>
-                            <x-bladewind.dropmenu-item>
-                                <p class="text-black">Logare</p>
-                            </x-bladewind.dropmenu-item>
-                            <x-bladewind.dropmenu-item>
-                                <p class="text-black">Inregistrare</p>
-                            </x-bladewind.dropmenu-item>
-                        </x-bladewind.dropmenu>
-
+            <x-bladewind.dropmenu>
+                <x-bladewind.dropmenu-item>
+                    <p class="text-black">Acasa</p>
+                </x-bladewind.dropmenu-item>
+                <x-bladewind.dropmenu-item>
+                    <p class="text-black">Despre noi</p>
+                </x-bladewind.dropmenu-item>
+                <x-bladewind.dropmenu-item>
+                    <p class="text-black">Contact</p>
+                </x-bladewind.dropmenu-item>
+                <x-bladewind.dropmenu-item>
+                    <p class="text-black">Logare</p>
+                </x-bladewind.dropmenu-item>
+                <x-bladewind.dropmenu-item>
+                    <p class="text-black">Inregistrare</p>
+                </x-bladewind.dropmenu-item>
+            </x-bladewind.dropmenu>
         </div>
 
     </div>
@@ -40,11 +39,19 @@
         </x-nav-link>
     </div>
     <div class="hidden lg:flex flex my-3">
+        @guest
         <x-nav-link href="" class="text-xl">
             Logare
         </x-nav-link>
         <x-nav-link href="" class="text-xl">
             Inregistrare
         </x-nav-link>
+        @endguest
+
+        @auth
+        <x-nav-link href="" class="text-xl">
+            Dashboard
+        </x-nav-link>
+        @endauth
     </div>
 </div>
