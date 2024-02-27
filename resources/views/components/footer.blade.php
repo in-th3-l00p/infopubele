ec94534a673223811d75ad8a6e4e2932672d8e52
nayrow <paseresebastian0504@gmail.com>
2024-02-27 15:34:49 +0200

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,34 @@
+<div class="bg-black h-48">
+    <div class="flex flex-col items-center pt-6 text-white">
+        <p class="mb-4">Infopubele.ro - Plătești cât arunci</p>
+        <hr width="40%" color="white">
+        <ol class="flex list-disc space-x-10 py-4" >
+            <li >
+                <x-nav-link class="text-[22px]">
+                    A.N.P.C.
+                </x-nav-link>
+            </li>
+            <li >
+                <x-nav-link class="text-[22px]">
+                    Termeni și condiții
+                </x-nav-link>
+            </li>
+            <li >
+                <x-nav-link class="text-[22px]">
+                    Prelucrarea datelor cu caracter personal
+                </x-nav-link>
+            </li>
+            <li >
+                <x-nav-link class="text-[22px]">
+                    Politica de cookies
+                </x-nav-link>
+            </li>
+        </ol>
+        <hr width="40%" color="white">
+        <div class="flex justify-center space-x-2 py-2">
+            <a href="#"><img class="h-12" src="/facebook.png" alt="a" /></a>
+            <a href="#"><img class="h-12" src="/instagram.png" alt="a"  /></a>
+        </div>
+
+    </div>
+</div>
