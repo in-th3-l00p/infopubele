2e03cbd4a4a617a799f8834b8096ae2c04036511
in-th3-l00p <indianucode0303@gmail.com>
2024-03-01 14:18:08 +0200

    fixed empty bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,46 +7,77 @@
                 <h1 class=" text-center text-5xl text-white font-bold drop-shadow-lg">BINE AI VENIT PE PLATFORMA
                     <span class="text-yellow-400 font-bold">INFOPUBELE</span>
                 </h1>
-                <p class="mt-5 text-center text-lg text-white ">Aceasta platforma este utitlizata pentru tranzactii cu pubele noastre</p>
+                <p class="mt-5 text-center text-lg text-white ">Aceasta platforma este utitlizata pentru tranzactii cu
+                    pubele noastre</p>
                 <a class="mt-8 px-12 py-3 bg-green-500 hover:from-amber-600 hover:to-red-700 text-xl text-white/70 font-semibold drop-shadow-lg rounded-full"
                    href="#">Continua</a>
             </div>
         </div>
     </div>
     <x-text-card>
-                    <h1 class=" mt-8 font-bold text-3xl">Reciclarea</h1>
-                    <br>
-                    <p>Reciclarea este un proces esențial pentru protejarea mediului înconjurător și pentru conservarea resurselor naturale. Este o practică prin care materialele utilizate sunt colectate, sortate, prelucrate și transformate în noi produse sau materii prime, în loc să fie aruncate la groapa de gunoi sau incinerate.</p>
-                    <br>
-                    <p>Importanța reciclării este una crucială în lupta împotriva poluării și schimbărilor climatice.</p>
-                    <br>
-                    <p>Iată câteva motive pentru care reciclarea este atât de importantă:</p>
-                    <br>
-                    <ol class="list-decimal ml-12 space-y-2">
-                        <li>Protejarea mediului: Reciclarea reduce cantitatea de deșeuri care ajung în depozitele de gunoi sau în natură. Deșeurile care nu sunt reciclate pot polua solul, apa și aerul, având un impact negativ asupra ecosistemelor și biodiversității.</li>
-                        <li>Economisirea resurselor naturale: Reciclarea reduce dependența de materii prime noi, cum ar fi lemnul, petrolul sau mineralele. Prin reciclare, putem extrage și utiliza materialele deja existente într-un ciclu continuu, evitând epuizarea resurselor naturale limitate ale Pământului.</li>
-                        <li>Reducerea emisiilor de gaze cu efect de seră: Producerea de noi produse din materiale reciclate necesită mai puțină energie decât producția din materii prime noi. Astfel, reciclarea contribuie la reducerea emisiilor de gaze cu efect de seră și la combaterea schimbărilor climatice.</li>
-                        <li>Economisirea energiei: Procesul de reciclare necesită, în general, mai puțină energie decât producția de materiale noi. De exemplu, reciclarea hârtiei poate economisi până la 60% din energia necesară pentru fabricarea hârtiei noi.</li>
-                        <li>Crearea de locuri de muncă și stimularea economiei circulare: Industria reciclării oferă oportunități de angajare și contribuie la dezvoltarea unei economii circulare, în care resursele sunt utilizate în mod eficient și durabil.</li>
-                    </ol>
-                    <br>
-                    <p>Pentru a maximiza beneficiile reciclării, este important să fim conștienți de impactul pe care îl avem asupra mediului și să adoptăm obiceiuri de consum responsabile. Sortarea corectă a deșeurilor și reciclarea lor în mod regulat sunt pași simpli pe care îi putem face pentru a contribui la protejarea mediului și la crearea unui viitor mai sustenabil.
-                    </p>
-                    <br>
-                    <p><b class="font-extrabold">Infopubele.ro</b> reprezintă o soluție inovatoare și eficientă pentru colectarea selectivă a deșeurilor, promovând transparența, responsabilitatea și implicarea fiecărei persoane în protejarea mediului înconjurător.
+        <h1 class=" mt-8 font-bold text-3xl">Reciclarea</h1>
+        <br>
+        <p>Reciclarea este un proces esențial pentru protejarea mediului înconjurător și pentru conservarea resurselor
+            naturale. Este o practică prin care materialele utilizate sunt colectate, sortate, prelucrate și
+            transformate în noi produse sau materii prime, în loc să fie aruncate la groapa de gunoi sau incinerate.</p>
+        <br>
+        <p>Importanța reciclării este una crucială în lupta împotriva poluării și schimbărilor climatice.</p>
+        <br>
+        <p>Iată câteva motive pentru care reciclarea este atât de importantă:</p>
+        <br>
+        <ol class="list-decimal ml-12 space-y-2">
+            <li>Protejarea mediului: Reciclarea reduce cantitatea de deșeuri care ajung în depozitele de gunoi sau în
+                natură. Deșeurile care nu sunt reciclate pot polua solul, apa și aerul, având un impact negativ asupra
+                ecosistemelor și biodiversității.
+            </li>
+            <li>Economisirea resurselor naturale: Reciclarea reduce dependența de materii prime noi, cum ar fi lemnul,
+                petrolul sau mineralele. Prin reciclare, putem extrage și utiliza materialele deja existente într-un
+                ciclu continuu, evitând epuizarea resurselor naturale limitate ale Pământului.
+            </li>
+            <li>Reducerea emisiilor de gaze cu efect de seră: Producerea de noi produse din materiale reciclate necesită
+                mai puțină energie decât producția din materii prime noi. Astfel, reciclarea contribuie la reducerea
+                emisiilor de gaze cu efect de seră și la combaterea schimbărilor climatice.
+            </li>
+            <li>Economisirea energiei: Procesul de reciclare necesită, în general, mai puțină energie decât producția de
+                materiale noi. De exemplu, reciclarea hârtiei poate economisi până la 60% din energia necesară pentru
+                fabricarea hârtiei noi.
+            </li>
+            <li>Crearea de locuri de muncă și stimularea economiei circulare: Industria reciclării oferă oportunități de
+                angajare și contribuie la dezvoltarea unei economii circulare, în care resursele sunt utilizate în mod
+                eficient și durabil.
+            </li>
+        </ol>
+        <br>
+        <p>Pentru a maximiza beneficiile reciclării, este important să fim conștienți de impactul pe care îl avem asupra
+            mediului și să adoptăm obiceiuri de consum responsabile. Sortarea corectă a deșeurilor și reciclarea lor în
+            mod regulat sunt pași simpli pe care îi putem face pentru a contribui la protejarea mediului și la crearea
+            unui viitor mai sustenabil.
+        </p>
+        <br>
+        <p><b class="font-extrabold">Infopubele.ro</b> reprezintă o soluție inovatoare și eficientă pentru colectarea
+            selectivă a deșeurilor, promovând transparența, responsabilitatea și implicarea fiecărei persoane în
+            protejarea mediului înconjurător.
 
-                    </p>
-                    <br>
-                    <p class="font-extrabold pb-10">Prin utilizarea tehnologiei și a unei abordări inovatoare, Infopubele.ro își propune să transforme gestionarea deșeurilor într-un proces simplu, inteligent și sustenabil.
+        </p>
+        <br>
+        <p class="font-extrabold pb-10">Prin utilizarea tehnologiei și a unei abordări inovatoare, Infopubele.ro își
+            propune să transforme gestionarea deșeurilor într-un proces simplu, inteligent și sustenabil.
 
-                    </p>
-        </x-text-card>
+        </p>
+    </x-text-card>
     <x-text-card>
-                <div class="ml-5 space-y-2">
-                    <h1 class=" mt-8 font-bold text-3xl">Locatia pubelelor</h1>
-                    <x-maps-leaflet :centerPoint="['lat' => $mid_point_x, 'long' => $mid_point_y]" :markers="$devices->map(fn (\App\Models\Device $device) => [$device->latitude, $device->longitude])"></x-maps-leaflet>
-
-                </div>
+        <div class="ml-5 space-y-2">
+            <h1 class="mt-8 font-bold text-3xl">Locatia pubelelor</h1>
+            <x-maps-leaflet
+                :centerPoint="['lat' => $mid_point_x, 'long' => $mid_point_y]"
+                @if ($devices->count() > 0)
+                    :markers="$devices->map(
+                            fn (\App\Models\Device $device) =>
+                                [$device->latitude, $device->longitude]
+                        )->all()"
+                @endif
+            />
+        </div>
     </x-text-card>
 
     <x-footer></x-footer>
