e3fe870171aa90cb1c07c8079a878257d9ac088a
nayrow <paseresebastian0504@gmail.com>
2024-03-08 10:34:46 +0200

    modified padding and shadows

--- resources/views/components/text-card.blade.php
+++ resources/views/components/text-card.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-gray-100">
     <div class="py-8 sm:mx-28 mx-8">
-        <div class="border rounded-2xl bg-white shadow-md">
-            <div class=" px-3 md:px-12 space-y-2">
+        <div class="border rounded-2xl bg-white shadow-xl">
+            <div class=" py-8 px-3 md:px-16 space-y-2">
                 {{$slot}}
             </div>
         </div>
