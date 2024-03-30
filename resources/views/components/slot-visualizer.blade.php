5fea94c408350f9c3ea576d12acb4708e6165f1d
nayrow <paseresebastian0504@gmail.com>
2024-03-30 14:56:41 +0200

    nu mai stiu

--- resources/views/components/slot-visualizer.blade.php
+++ resources/views/components/slot-visualizer.blade.php
@@ -3,7 +3,7 @@
 
     <div class="md:ml-4 gap-2 flex flex-col items-center md:flex-row md:justify-between">
         @forelse($device->slots()->get() as $slot)
-            <div class="relative bg-white border-2 border-t-gray-50 flex flex-col justify-center items-center w-32 h-32 rounded-bl-lg rounded-br-lg z-10">
+            <div class="relative bg-white border-2 border-t-gray-50 flex flex-col md:flex-row justify-center items-center w-32 h-32 rounded-bl-lg rounded-br-lg z-10">
                 <p>{{ __("Slot ") . $slot->name }}</p>
 
                 @php $percentage = $slot->volume / $slot->max_volume * 100;  @endphp
