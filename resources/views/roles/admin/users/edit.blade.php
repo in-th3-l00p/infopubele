ee380ffca56561c29f8dbaa54410f2fc34cadaaa
nayrow <paseresebastian0504@gmail.com>
2024-04-23 17:08:49 +0300

    build the front-end and back-end for editing user with types

--- resources/views/roles/admin/users/edit.blade.php
+++ resources/views/roles/admin/users/edit.blade.php
@@ -13,9 +13,7 @@
     <x-white-container>
         <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('users.update', [
-            "user" => $user
-        ]) }}">
+        <form id="editForm" method="POST" action="{{ route('users.update', ['user' => $user]) }}">
             @csrf
             @method("PUT")
 
@@ -60,18 +58,66 @@
                 </select>
             </div>
 
-            <div class="mt-4">
+            <div id="typeSelection" class="mt-4" style="display: none;">
                 <x-label for="type" value="{{ __('Tip') }}" />
                 <select
                     id="type"
                     name="type"
                     class="select"
                 >
+                    <option value="" selected>{{__("Alege")}}</option>
                     <option value="individual" @selected($user->type === "individual") >Individual</option>
                     <option value="legal-entity" @selected($user->type === "legal-entity")>Legal Entity</option>
                     <option value="homeowners-association" @selected($user->type === "homeowners-association")>Homeowners Association</option>
                 </select>
             </div>
+
+            <div id="individualSection" class="mt-4" @if($user->type === 'individual') style="display: block;" @else style="display: none;" @endif>
+                <div class="mt-4">
+                    <x-label for="cnp" value="{{ __('CNP') }}" />
+                    <x-input id="cnp" class="block mt-1 w-full" type="text" name="cnp" :value="$user->cnp" required autocomplete="cnp" />
+                </div>
+            </div>
+
+            <div id="legalEntitySection" class="mt-4" @if($user->type === 'legal-entity') style="display: block;" @else style="display: none;" @endif>
+                <div class="mt-4">
+                    <x-label for="cui" value="{{ __('CUI') }}" />
+                    <x-input id="cui" class="block mt-1 w-full" type="text" name="cui" :value="$user->cui" required autocomplete="cui" />
+                </div>
+            </div>
+
+            <div id="hoaSection" class="mt-4" @if($user->type === 'homeowners-association') style="display: block;" @else style="display: none;" @endif>
+                <div class="mt-4">
+                    <x-label for="cui" value="{{ __('CUI') }}" />
+                    <x-input id="cui" class="block mt-1 w-full" type="text" name="cui" :value="$user->cui" required autocomplete="cui" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="address" value="{{ __('Adresa') }}" />
+                    <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="$user->address" required autocomplete="address" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="contract_number" value="{{ __('Număr contract') }}" />
+                    <x-input id="contract_number" class="block mt-1 w-full" type="text" name="contract_number" :value="$user->contract_number" required autocomplete="contract_number" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="contact_person" value="{{ __('Persoană de contact') }}" />
+                    <x-input id="contact_person" class="block mt-1 w-full" type="text" name="contact_person" :value="$user->contact_person" required autocomplete="contact_person" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="phone" value="{{ __('Telefon') }}" />
+                    <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="$user->phone" required autocomplete="phone" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="inhabitants" value="{{ __('Număr locuitori') }}" />
+                    <x-input id="inhabitants" class="block mt-1 w-full" type="text" name="inhabitants" :value="$user->inhabitants" required autocomplete="inhabitants" />
+                </div>
+            </div>
+
 
             <div class="mt-4">
                 <x-label for="city" value="{{ __('Oraș') }}" />
@@ -89,9 +135,9 @@
                     @foreach($cities as $city)
                         @if($city->name !== $user->city)
                             <option value="{{$city->name}}" >{{$city->name}}</option>
-                            @else
-                                <option value="{{$user->city}}" selected>{{$user->city}}</option>
-                            @endif
+                        @else
+                            <option value="{{$user->city}}" selected>{{$user->city}}</option>
+                        @endif
                     @endforeach
                 </select>
                 <x-input-error for="city" class="mt-2" />
@@ -118,8 +164,75 @@
             @endif
 
             <div class="flex items-center justify-center mt-4">
-                <x-button>{{ __('Salvează') }}</x-button>
+                <x-button id="submitButton">{{ __('Salvează') }}</x-button>
             </div>
         </form>
     </x-white-container>
 </x-app-layout>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var roleSelect = document.getElementById('role');
+        var typeSelect = document.getElementById('type');
+        var individualSection = document.getElementById('individualSection');
+        var legalEntitySection = document.getElementById('legalEntitySection');
+        var hoaSection = document.getElementById('hoaSection');
+        var submitButton = document.getElementById('submitButton');
+
+        function showTypeSelection() {
+            var selectedRole = roleSelect.value;
+            if (selectedRole === 'user') {
+                typeSelection.style.display = 'block';
+            } else {
+                typeSelection.style.display = 'none';
+                // Reset type selection to "Alege" when role changes from user
+                typeSelect.value = '';
+                // Hide all sections when role changes
+                individualSection.style.display = 'none';
+                legalEntitySection.style.display = 'none';
+                hoaSection.style.display = 'none';
+            }
+        }
+
+        function showTypeContent() {
+            var selectedType = typeSelect.value;
+            // Hide all sections first
+            individualSection.style.display = 'none';
+            legalEntitySection.style.display = 'none';
+            hoaSection.style.display = 'none';
+            // Show the section based on the selected type
+            if (selectedType === 'individual') {
+                individualSection.style.display = 'block';
+            } else if (selectedType === 'legal-entity') {
+                legalEntitySection.style.display = 'block';
+            } else if (selectedType === 'homeowners-association') {
+                hoaSection.style.display = 'block';
+            }
+        }
+
+        showTypeSelection(); // Show initially based on default selected value
+
+        roleSelect.addEventListener('change', function() {
+            showTypeSelection();
+            // Hide content when role changes
+            if (roleSelect.value !== 'user') {
+                typeSelection.style.display = 'none';
+                // Reset type selection to "Alege" when role changes from user
+                typeSelect.value = '';
+                // Hide all sections when role changes
+                individualSection.style.display = 'none';
+                legalEntitySection.style.display = 'none';
+                hoaSection.style.display = 'none';
+            }
+        });
+
+        typeSelect.addEventListener('change', showTypeContent);
+
+        submitButton.addEventListener('click', function() {
+            // Make sure to display the selected type content before submitting
+            showTypeContent();
+            document.getElementById('editForm').submit();
+        });
+    });
+</script>
+
