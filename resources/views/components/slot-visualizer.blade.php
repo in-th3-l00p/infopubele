6538977e7b55f5604acaf66c8294d264a3518e6c
nayrow <paseresebastian0504@gmail.com>
2024-05-05 09:53:28 +0300

    updated visualizer for slots

--- resources/views/components/slot-visualizer.blade.php
+++ resources/views/components/slot-visualizer.blade.php
@@ -1,7 +1,7 @@
 <x-white-container>
-    <h2 class="text-lg font-semibold mb-8">{{ __("Vizualizare") }}</h2>
+    <h2 class="text-lg text-center md:text-left font-semibold mb-8">{{ __("Vizualizare") }}</h2>
 
-    <div class="md:ml-4 gap-2 flex flex-col items-center md:flex-row md:justify-between">
+    <div class="md:ml-4 gap-2 flex flex-col items-center md:flex-row md:gap-8 md:justify-center">
         @forelse($device->slots()->get() as $slot)
             <div class="relative bg-white border-2 border-t-gray-50 flex gap-2 flex-col text-center justify-center items-center w-32 h-32 rounded-bl-lg rounded-br-lg z-10">
                 <p>{{ __("Slot ") . $slot->name }}</p>
