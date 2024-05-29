0c1b3dde7175d1de68177e57619c6aececf3a0de
nayrow <paseresebastian0504@gmail.com>
2024-05-29 14:53:26 +0300

    merge conflict

--- app/Http/Controllers/Api/CardController.php
+++ app/Http/Controllers/Api/CardController.php
@@ -4,20 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Card;
-use App\Models\User;
+use App\Models\Device;
 use Illuminate\Http\Request;
 
 class CardController extends Controller
 {
     public function index(Request $request) {
-        $device = ApiSlotController::getDevice($request);
-        $request->validate([
-            "token" => "required|exists:device_tokens,token"
-        ], [
-            "token.required" => "Tokenul este obligatoriu",
-            "token.exists" => "Token invalid"
-        ]);
-
+        $device = Device::query()->findOrFail($request->device_id);
         $query = Card::query()
             ->with("user")
             ->with("device");
@@ -27,67 +20,40 @@ class CardController extends Controller
             $query->where("uuid", $request->card_id);
         if ($request->has("device_id"))
             $query->where("device_id", $device->id);
+        if ($request->has("slot_id"))
+            $query->whereHas("device.slots",
+                function ($query) use ($request) {
+                    $query->where("id", $request->slot_id);
+                });
         return $query->latest()->get();
     }
 
     public function store(Request $request) {
-        $device = ApiSlotController::getDevice($request);
-        $user_id = User::query()->where("contract_number",$request->contract_number)->first()->id;
+        $device = Device::query()->findOrFail($request->device_id);
         $request->validate([
-            "contract_number" => "required|exists:users,contract_number",
+            "user_id" => "required|exists:users,id",
         ],[
-            "contract_number.required" => "Contractul este obligatoriu",
-            "contract_number.exists" => "Contractul nu exista"
+            "user_id.required" => "Userul este obligatoriu",
+            "user_id.exists" => "Userul nu exista"
         ]);
-        if (Card::where("user_id", $user_id)
+        if (Card::where("user_id", $request->user_id)
             ->where("device_id", $device->id)
             ->exists())
             return response()->json([
                 "message" => "Card already exists"
             ], 400);
         return Card::create([
-            "user_id" => $user_id,
+            "user_id" => $request->user_id,
             "device_id" => $device->id,
             "uuid" => \Illuminate\Support\Str::uuid()->toString()
         ]);
     }
 
     public function show(Request $request, Card $card) {
-        $request->validate([
-            "token" => "required|exists:device_tokens,token"
-        ],[
-            "token.required" => "Tokenul este obligatoriu",
-            "token.exists" => "Token invalid"
-        ]);
-        if (!$card->device()->first()
-            ->tokens()
-            ->where("token", $request->token)
-            ->exists()
-        )
-            return response()->json([
-                "message" => "Invalid token"
-            ], 401);
+        $device = Device::query()->findOrFail($request->device_id);
         return $card->load("user", "device");
     }
 
-    public function destroy(Request $request, Card $card) {
-        $request->validate([
-            "token" => "required|exists:device_tokens,token"
-        ],[
-            "token.required" => "Tokenul este obligatoriu",
-            "token.exists" => "Token invalid"
-        ]);
-        if (!$card->device()->first()
-            ->tokens()
-            ->where("token", $request->token)
-            ->exists()
-        )
-            return response()->json([
-                "message" => "Invalid token"
-            ], 401);
-        $card->delete();
-        return response()->json([
-            "message" => "Card deleted"
-        ]);
-    }
+    // no need for implementation
+    public function destroy(Request $request, Card $card) { }
 }
