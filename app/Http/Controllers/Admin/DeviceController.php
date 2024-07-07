58a90090e7655256824ea231d4691069723a9692
nayrow <paseresebastian0504@gmail.com>
2024-07-07 19:24:40 +0300

    number of cards api endpoint

--- app/Http/Controllers/Admin/DeviceController.php
+++ app/Http/Controllers/Admin/DeviceController.php
@@ -70,6 +70,13 @@ class DeviceController extends Controller
         return response("", 200);
     }
 
+    public function cardsNumber(Request $request, Device $device)
+    {
+        return response()->json([
+            "cards" => $device->cards()->count()
+        ]);
+    }
+
     public function destroy(Device $device)
     {
         $device->delete();
