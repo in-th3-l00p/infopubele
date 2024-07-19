845b34c7c3545853b2b8f63c5add1879289ad347
nayrow <paseresebastian0504@gmail.com>
2024-07-19 13:56:17 +0300

    migration for slots

---
+++ database/migrations/2024_07_19_085631_updated_slots_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("slots", function (Blueprint $table) {
+            $table->enum("type",["paper/cardboard","residual","biodegradable","plastic/metal"])->nullable();
+        });
+    }
+};
