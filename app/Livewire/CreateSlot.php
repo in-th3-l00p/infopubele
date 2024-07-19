6e32a10010d43555b80792e4ca9899b6f921dce6
nayrow <paseresebastian0504@gmail.com>
2024-07-19 13:56:02 +0300

    implemented slot type

--- app/Livewire/CreateSlot.php
+++ app/Livewire/CreateSlot.php
@@ -4,12 +4,14 @@ namespace App\Livewire;
 
 use App\Models\Device;
 use App\Models\Slot;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class CreateSlot extends Component
 {
     public Device $device;
     public string $name;
+    public string $type;
     public float $capacity;
 
     public function render()
@@ -20,7 +22,14 @@ class CreateSlot extends Component
     public function createSlot() {
         $this->validate([
             "name" => "required|min:3|max:255|unique:slots,name,NULL,id,device_id," . $this->device->id,
-            "capacity" => "required|numeric|min:1|max:1100"
+            "capacity" => "required|numeric|min:1|max:1100",
+            "type" => [
+                "required",
+                "in:paper/cardboard,residual,biodegradable,plastic/metal",
+                Rule::unique('slots', 'type')->where(function ($query) {
+                    return $query->where('device_id', $this->device->id);
+                }),
+            ]
         ], [
             "name.unique" => __("Numele trebuie să fie unic"),
             "name.required" => __("Numele este obligatoriu"),
@@ -30,7 +39,11 @@ class CreateSlot extends Component
 
             "capacity.numeric" => __("Capacitatea trebuie să fie un număr"),
             "capacity.min" => __("Capacitatea trebuie să fie de cel puțin 1"),
-            "capacity.max" => __("Capacitatea trebuie să fie de cel mult 1100")
+            "capacity.max" => __("Capacitatea trebuie să fie de cel mult 1100"),
+
+            "type.in" => __("Tipul trebuie să fie unul dintre: hartie/carton, rezidual, biodegradabil, plastic/metal"),
+            "type.required" => __("Tipul este obligatoriu"),
+            "type.unique" => __("Nu pot fi 2 sloturi de acelasi tip pentru un dispozitiv")
         ]);
 
         $slot = Slot::create([
@@ -38,7 +51,8 @@ class CreateSlot extends Component
             "max_volume" => $this->capacity,
             "volume" => 0,
             "device_id" => $this->device->id,
-            "order" => $this->device->slots()->count()
+            "order" => $this->device->slots()->count(),
+            "type" => $this->type
         ]);
 
         if(auth()->user()->role === "admin") {
--- resources/views/livewire/create-slot.blade.php
+++ resources/views/livewire/create-slot.blade.php
@@ -9,23 +9,41 @@
 
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Nume') }}" />
+            <x-label for="name" value="{{ __('Nume') }}"/>
             <x-input
                 id="name" type="text" class="mt-1 block w-full"
                 required autocomplete="name"
                 wire:model="name"
             />
-            <x-input-error for="name" class="mt-2" />
+            <x-input-error for="name" class="mt-2"/>
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="capacity" value="{{ __('Capacitate Maximă(L)') }}" />
+            <x-label for="capacity" value="{{ __('Capacitate Maximă(L)') }}"/>
             <x-input
                 id="capacity" type="number" class="mt-1 block w-full"
                 required autocomplete="capacity"
                 wire:model="capacity"
             />
-            <x-input-error for="capacity" class="mt-2" />
+            <x-input-error for="capacity" class="mt-2"/>
+        </div>
+
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="type" value="{{ __('Tipul Slotului') }}" />
+            <select
+                id="type"
+                name="type"
+                class="select"
+                required
+                wire:model="type"
+            >
+                <option value="">{{ __('Select Type') }}</option>
+                <option value="paper/cardboard">Hartie/Carton</option>
+                <option value="residual">Rezidual</option>
+                <option value="biodegradable">Biodegradabil</option>
+                <option value="plastic/metal">Plastic/Metal</option>
+            </select>
+            <x-input-error for="type" class="mt-2"/>
         </div>
     </x-slot>
 
