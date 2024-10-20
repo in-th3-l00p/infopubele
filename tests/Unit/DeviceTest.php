b68542c74eca9671e0d3a867e3548eee18ac3627
in-th3-l00p <indianucode0303@gmail.com>
2024-10-20 12:33:53 +0300

    tests for device

--- tests/Unit/DeviceTest.php
+++ tests/Unit/DeviceTest.php
@@ -5,14 +5,37 @@ namespace Tests\Unit\Models;
 use App\Models\Device;
 use App\Models\User;
 use Database\Seeders\DatabaseSeeder;
+use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class DeviceTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
+    #[Test]
+    public function it_requires_unique_name_and_series() {
+        $this->seed(DatabaseSeeder::class);
+        $device = Device::factory()->create([
+            'name' => 'Device 1',
+            'series' => '123456',
+        ]);
+
+        $this->assertDatabaseHas('devices', [
+            'name' => 'Device 1',
+            'series' => '123456'
+        ]);
+
+        $this->expectException(UniqueConstraintViolationException::class);
+
+        Device::factory()->create([
+            'name' => 'Device 1',
+            'series' => '123456'
+        ]);
+    }
+
+    #[Test]
     public function it_has_many_associated_users()
     {
         $user = User::factory()->create();
@@ -24,7 +47,7 @@ class DeviceTest extends TestCase
         $this->assertTrue($device->associatedUser->contains($user));
     }
 
-    /** @test */
+    #[Test]
     public function it_belongs_to_an_owner()
     {
         $owner = User::factory()->create();
