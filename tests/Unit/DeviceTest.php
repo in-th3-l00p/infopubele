2fef48c9fc4c351f7ff6aefaa4e5591dd6a5dc38
in-th3-l00p <indianucode0303@gmail.com>
2024-10-20 15:54:13 +0300

    DeviceSeeder

--- tests/Unit/DeviceTest.php
+++ tests/Unit/DeviceTest.php
@@ -4,7 +4,7 @@ namespace Tests\Unit\Models;
 
 use App\Models\Device;
 use App\Models\User;
-use Database\Seeders\DatabaseSeeder;
+use Database\Seeders\UserSeeder;
 use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
@@ -16,7 +16,7 @@ class DeviceTest extends TestCase
 
     #[Test]
     public function it_requires_unique_name_and_series() {
-        $this->seed(DatabaseSeeder::class);
+        $this->seed(UserSeeder::class);
         $device = Device::factory()->create([
             'name' => 'Device 1',
             'series' => '123456',
@@ -38,7 +38,7 @@ class DeviceTest extends TestCase
     #[Test]
     public function it_has_many_associated_users()
     {
-        $this->seed(DatabaseSeeder::class);
+        $this->seed(UserSeeder::class);
         $device = Device::factory()->create();
         $user = User::query()
             ->where("role", "=", "admin")
@@ -53,7 +53,7 @@ class DeviceTest extends TestCase
     #[Test]
     public function it_belongs_to_many_associated_users()
     {
-        $this->seed(DatabaseSeeder::class);
+        $this->seed(UserSeeder::class);
         $device = Device::factory()->create();
         $users = User::query()->inRandomOrder()->get();
         $device->associatedUsers()->attach($users[0]);
