add065a32a689d7de19efba261312a2a809a7027
in-th3-l00p <indianucode0303@gmail.com>
2024-10-21 22:03:17 +0300

    logheeaza-te, trebuie sa imi schimb ochelarii

--- resources/views/components/landing/hero.blade.php
+++ resources/views/components/landing/hero.blade.php
@@ -14,7 +14,7 @@
                             href="{{ route("login") }}"
                             class="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                         >
-                            Logheaază-te
+                            Loghează-te
                         </a>
                     @endguest
                     @auth
